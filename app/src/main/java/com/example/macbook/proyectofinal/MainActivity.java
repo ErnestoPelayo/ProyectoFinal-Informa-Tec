f4d002edfabddf94c50c718f09671ce0c58a0cdf
Ernesto Pelayo <ernesto.p@itemexicali.edu.mx>
2018-10-28 17:18:19 -0700

    se agregan fragments y se eliminan activitys

--- app/src/main/java/com/example/macbook/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/macbook/proyectofinal/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.macbook.proyectofinal;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private ConferenceFragment conferenceFragment;
+    private ContestFragment contestFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        contestFragment = new ContestFragment();
+        conferenceFragment = new ConferenceFragment();
+        homeFragment = new HomeFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.nav_home:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.nav_contest:
+                        mMainNav.setItemBackgroundResource(R.color.colorContest);
+                        setFragment(contestFragment);
+                        return true;
+
+                    case R.id.nav_conference:
+                        mMainNav.setItemBackgroundResource(R.color.coloConferences);
+                        setFragment(conferenceFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
     }
 }
